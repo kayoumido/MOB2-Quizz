4c7c119a106f4b47ab37a66d6e4b8f3970874f63
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-02-12 11:45:45 +0100

    QuizSession changed to protocol

--- Quiz/QuizSession.swift
+++ Quiz/QuizSession.swift
@@ -8,71 +8,11 @@
 
 import Foundation
 
-class QuizSession {
+protocol QuizSession {
+    var score: Int { get }
+    var questionCount: Int { get }
+    var currentQuestion: Question { get }
     
-    fileprivate var questions: [Question]
-    fileprivate var currentIndex: Int
-    private var _score: Int
-    
-    var score: Int {
-        get {
-            return self._score
-        }
-    }
-    
-    var questionCount: Int {
-        get {
-            return self.questions.count
-        }
-    }
-    
-    var currentQuestion: Question {
-        get {
-            return self.questions[currentIndex]
-        }
-    }
-    
-    init() {
-        questions = [
-            Question(
-                caption: "2+2",
-                answers: ["1", "2", "4"],
-                correctAnswer: "4",
-                hint: "A toddler could answer these questions...."
-            ),
-            Question(
-                caption: "Meaning of life?",
-                answers: ["God", "42", "Me"],
-                correctAnswer: "42",
-                hint: "Is there really a meaning to life ?"
-            ),
-            Question(
-                caption: "May the Force be with you",
-                answers: ["Star wars", "Forest Gump", "American pie"],
-                correctAnswer: "Star wars",
-                hint: "You for real ?"
-            )
-        ]
-        
-        currentIndex = -1
-        _score = 0
-    }
-    
-    func nextQuestion() -> Question? {
-        if currentIndex+1 >= questions.count {
-            return nil
-        }
-        else {
-            currentIndex += 1
-            return questions[currentIndex]
-        }
-    }
-    
-    func checkAnswer(_ answer: String) -> Bool {
-        let result: Bool = questions[currentIndex].isCorrectAnswer(answer)
-        if result {
-            self._score += 1
-        }
-        return result
-    }
+    func nextQuestion() -> Question?
+    func checkAnswer(_ answer: String) -> Bool
 }
