61725806f54d4410a52f0bed82efe7c744b20c72
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-03-01 11:54:57 +0100

    create WarriorQuizSession

---
+++ Quiz/QuizSession/WarriorQuizSession.swift
@@ -0,0 +1,47 @@
+//
+//  WarriorQuizSession.swift
+//  Quiz
+//
+//  Created by Doran Kayoumi on 26.02.18.
+//  Copyright © 2018 Pascal Hurni. All rights reserved.
+//
+
+import Foundation
+
+class WarriorQuizSession : QuizSession {
+    
+    var _currentQuestionCount: Int
+    private var _wasLastQuestionCorrect: Bool
+    
+    init(questionRepository: QuestionRepository) {
+        self._currentQuestionCount = 1
+        self._wasLastQuestionCorrect = true
+        
+        super.init(questionRepository: questionRepository, totalQuestionCount: 15, time: 30)
+    }
+    
+    override func nextQuestion() -> Question? {
+        
+        if self._currentQuestionCount > _totalQuestionCount {
+            return nil
+        }
+        
+        if !self._wasLastQuestionCorrect {
+            return _currentQuestion
+        }
+        self._currentQuestionCount += 1
+        return super.nextQuestion()
+    }
+    
+    override func checkAnswer(_ answer: String) -> Bool {
+        self._wasLastQuestionCorrect = super.checkAnswer(answer)
+        if self._wasLastQuestionCorrect {
+            _score += 1
+        }
+        else {
+            _score -= 1
+        }
+        
+        return self._wasLastQuestionCorrect
+    }
+}
